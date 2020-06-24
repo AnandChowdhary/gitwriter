e53c778cbbe4ae01c7a885c3fbc48d1e2dcab636
Anand Chowdhary <mail@anandchowdhary.com>
2020-06-24 15:51:12 +0530

    :bug: Remove console log

--- src/views/Repos.vue
+++ src/views/Repos.vue
@@ -64,7 +64,7 @@ export default class Home extends Vue {
         }
         this.filter();
       })
-      .catch(error => console.log(error))
+      .catch(error => {})
       .then(() => (this.loading = false));
   }
   encode(data: string) {
